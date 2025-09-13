df3a8060ffdf0077e40c1804921240d137e6c268
unknown <luthfirobbaniy@gmail.com>
2025-09-14 00:39:19 +0700

    feat(content,track): update model

--- internal/content/content.go
+++ internal/content/content.go
@@ -1,14 +1,22 @@
 package content
 
 import (
-	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/internal/track"
+	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/internal/practice"
 	"gorm.io/gorm"
 )
 
 type Content struct {
 	gorm.Model
-	TrackID uint        `json:"track_id"`
-	Track   track.Track `json:"track"`
-	Order   uint        `json:"order"`
-	Type    string      `json:"type"`
+	TrackID  uint               `json:"trackId"`
+	Order    uint               `json:"order"`
+	Type     ContentType        `json:"type" gorm:"type:content_types"`
+	Practice *practice.Practice `json:"practice"`
 }
+
+type ContentType string
+
+const (
+	ContentTypeMaterial ContentType = "material"
+	ContentTypePractice ContentType = "practice"
+	ContentTypeProject  ContentType = "project"
+)
--- internal/track/track.go
+++ internal/track/track.go
@@ -1,10 +1,14 @@
 package track
 
 import (
+	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/internal/content"
+	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/internal/practice"
 	"gorm.io/gorm"
 )
 
 type Track struct {
 	gorm.Model
-	Name string `json:"name"`
+	Name      string               `json:"name"`
+	Contents  *[]content.Content   `json:"-"`
+	Practices *[]practice.Practice `json:"-"`
 }
