6eff9dd62f9a44443c587ff2d8fe14d06c0347bd
unknown <luthfirobbaniy@gmail.com>
2025-09-14 21:48:02 +0700

    refactor(practice): new practice feature models schemas

--- internal/content/content.go
+++ internal/content/content.go
@@ -1,16 +1,18 @@
 package content
 
 import (
-	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/internal/practice"
+	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/internal/question"
 	"gorm.io/gorm"
 )
 
 type Content struct {
 	gorm.Model
-	TrackID  uint               `json:"trackId"`
-	Order    uint               `json:"order"`
-	Type     ContentType        `json:"type" gorm:"type:content_types"`
-	Practice *practice.Practice `json:"practice"`
+	TrackID   uint                 `json:"trackId"`
+	Title     string               `json:"title"`
+	Body      string               `json:"body"`
+	Type      ContentType          `json:"type" gorm:"type:content_types"`
+	Order     uint                 `json:"order"`
+	Questions *[]question.Question `json:"questions"`
 }
 
 type ContentType string
--- internal/track/track.go
+++ internal/track/track.go
@@ -2,13 +2,11 @@ package track
 
 import (
 	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/internal/content"
-	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/internal/practice"
 	"gorm.io/gorm"
 )
 
 type Track struct {
 	gorm.Model
-	Name      string               `json:"name"`
-	Contents  *[]content.Content   `json:"-"`
-	Practices *[]practice.Practice `json:"-"`
+	Name     string             `json:"name"`
+	Contents *[]content.Content `json:"-"`
 }
