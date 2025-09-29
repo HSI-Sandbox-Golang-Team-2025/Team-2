1b781c862c51561ea78a82f32e2f19de8cf7e307
unknown <luthfirobbaniy@gmail.com>
2025-09-29 16:23:37 +0700

    feat(api): vercel handler and config

---
+++ api/index.go
@@ -0,0 +1,158 @@
+package handler
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/docs"
+	authHandler "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/auth/handler"
+	authRepository "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/auth/repository"
+	authService "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/auth/service"
+	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/content"
+	contentHandler "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/content/handler"
+	contentRepository "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/content/repository"
+	contentService "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/content/service"
+	practiceHandler "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/practice/handler"
+	practiceService "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/practice/service"
+	projectHandler "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/project/handler"
+	projectService "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/project/service"
+	userHandler "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/user/handler"
+	userRepository "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/user/repository"
+	userService "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/user/service"
+	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/user_practice"
+	userPracticeHandler "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/user_practice/handler"
+	userPracticeRepository "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/user_practice/repository"
+	userPracticeService "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/user_practice/service"
+	userPracticeRecordRepository "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/user_practice_record/repository"
+	userProjectHandler "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/user_project/handler"
+	userProjectRepository "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/user_project/repository"
+	userProjectService "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/user_project/service"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	fiberLogger "github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/swagger"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	r.RequestURI = r.URL.String()
+	handler().ServeHTTP(w, r)
+}
+
+// @title Learning Platform
+// @version 1.0
+// @description Learning Platform API Documentations
+// @host localhost:3000
+// @BasePath /api
+// @schemes http
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+// @description Type "Bearer" followed by a space and JWT token.
+func handler() http.HandlerFunc {
+	app := fiber.New(fiber.Config{
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			code := fiber.StatusInternalServerError
+			message := err.Error()
+			// message := "Internal server error" // Production
+
+			if e, ok := err.(*fiber.Error); ok {
+				if e.Code != fiber.StatusInternalServerError {
+					code = e.Code
+					message = e.Message
+				}
+			}
+
+			return c.Status(code).JSON(fiber.Map{
+				"message": message,
+			})
+		},
+	})
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.JSON(fiber.Map{
+			"message": "Welcome!",
+		})
+	})
+
+	db, err := gorm.Open(postgres.Open("postgresql://learning_platform_jrke_user:2IBrCpndKXHWQrPIBaqtFkit4kx24LAG@dpg-d3cl8jndiees738313og-a.singapore-postgres.render.com/learning_platform_jrke"), &gorm.Config{
+		// Logger: gormLogger.Default.LogMode(gormLogger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database ", err)
+	}
+
+	db.Exec(
+		fmt.Sprintf(
+			"CREATE TYPE content_types AS ENUM ('%s', '%s', '%s');",
+			content.ContentTypeMaterial,
+			content.ContentTypePractice,
+			content.ContentTypeProject,
+		),
+	)
+
+	db.Exec(
+		fmt.Sprintf(
+			"CREATE TYPE user_practice_status AS ENUM ('%s', '%s', '%s', '%s');",
+			user_practice.Opened,
+			user_practice.InProgress,
+			user_practice.Submitted,
+			user_practice.Reviewed,
+		),
+	)
+
+	// err = db.AutoMigrate(
+	// 	&track.Track{},
+	// 	&content.Content{},
+	// 	&question.Question{},
+	// 	&question_answer_choice.QuestionAnswerChoice{},
+	// 	&role.Role{},
+	// 	&user.User{},
+	// 	&user_practice.UserPractice{},
+	// 	&user_practice_record.UserPracticeRecord{},
+	// 	&user_project.UserProject{},
+	// 	&user_project_media.UserProjectMedia{},
+	// 	&user_track.UserTrack{},
+	// )
+
+	// if err != nil {
+	// 	log.Fatal("Auto migration failed ", err)
+	// }
+
+	app.Use(fiberLogger.New())
+
+	route := app.Group("/api")
+
+	// Create new repos
+	authRepo := authRepository.NewRepository(db)
+	contentRepo := contentRepository.NewRepository(db)
+	userPracticeRepo := userPracticeRepository.NewRepository(db)
+	userPracticeRecordRepo := userPracticeRecordRepository.NewRepository(db)
+	userProjectRepo := userProjectRepository.NewRepository(db)
+	userRepo := userRepository.NewRepository(db)
+
+	// Create new services
+	authSvc := authService.NewService(authRepo, userRepo)
+	contentSvc := contentService.NewService(contentRepo, userPracticeRepo)
+	practiceSvc := practiceService.NewService(contentRepo)
+	projectSvc := projectService.NewService(contentRepo)
+	userPracticeSvc := userPracticeService.NewService(userPracticeRepo, userPracticeRecordRepo)
+	userProjectSvc := userProjectService.NewService(userProjectRepo)
+	userSvc := userService.NewService(userRepo)
+
+	// Create new handlers
+	authHandler.NewHandler(route, authSvc)
+	contentHandler.NewHandler(route, contentSvc)
+	practiceHandler.NewHandler(route, practiceSvc)
+	projectHandler.NewHandler(route, projectSvc)
+	userPracticeHandler.NewHandler(route, userPracticeSvc)
+	userProjectHandler.NewHandler(route, userProjectSvc)
+	userHandler.NewHandler(route, userSvc)
+
+	app.Get("/swagger/*", swagger.HandlerDefault)
+
+	return adaptor.FiberApp(app)
+}
