8f6ffa0bff88315880d92a1d3a00bb21dc00c596
unknown <luthfirobbaniy@gmail.com>
2025-10-04 23:35:44 +0700

    refactior(basic_model): change BasicModel.DeletedAt type to time.Time

--- pkg/basic_model/basic_model.go
+++ pkg/basic_model/basic_model.go
@@ -2,13 +2,11 @@ package basic_model
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type BasicModel struct {
-	ID        uint           `json:"id"`
-	CreatedAt time.Time      `json:"createdAt" gorm:"default:now()"`
-	UpdatedAt time.Time      `json:"updatedAt" gorm:"default:now()"`
-	DeletedAt gorm.DeletedAt `json:"deletedAt"`
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createdAt" gorm:"default:now()"`
+	UpdatedAt time.Time `json:"updatedAt" gorm:"default:now()"`
+	DeletedAt time.Time `json:"deletedAt"`
 }
