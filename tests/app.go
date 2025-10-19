3790d32a1d09de6dcf1c0aed1274e1112ccc34bf
unknown <luthfirobbaniy@gmail.com>
2025-10-19 23:27:05 +0700

    feat(test): update with the new main.go code

--- tests/app.go
+++ tests/app.go
@@ -5,6 +5,8 @@ import (
 	"log"
 
 	_ "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/docs"
+	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/middleware"
+	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/acl"
 	authHandler "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/auth/handler"
 	authRepository "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/auth/repository"
 	authService "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/auth/service"
@@ -12,13 +14,20 @@ import (
 	contentHandler "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/content/handler"
 	contentRepository "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/content/repository"
 	contentService "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/content/service"
+	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/endpoint"
+	endpointRepository "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/endpoint/repository"
 	practiceHandler "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/practice/handler"
 	practiceService "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/practice/service"
 	projectHandler "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/project/handler"
 	projectService "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/project/service"
 	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/question"
+	questionHandler "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/question/handler"
+	questionRepository "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/question/repository"
+	questionService "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/question/service"
 	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/question_answer_choice"
 	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/role"
+	roleRepository "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/role/repository"
+	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/seeder"
 	"github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/track"
 	trackHandler "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/track/handler"
 	trackRepository "github.com/HSI-Sandbox-Golang-Team-2025/Team-2/pkg/track/repository"
@@ -52,6 +61,16 @@ import (
 	gormLogger "gorm.io/gorm/logger"
 )
 
+// @title Learning Platform
+// @version 1.0
+// @description Learning Platform API Documentations
+// @host localhost:3000
+// @BasePath /api
+// @schemes http
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+// @description Type "Bearer" followed by a space and JWT token.
 func SetupApp() *fiber.App {
 	app := fiber.New(fiber.Config{
 		ErrorHandler: func(c *fiber.Ctx, err error) error {
@@ -96,8 +115,7 @@ func SetupApp() *fiber.App {
 
 	db.Exec(
 		fmt.Sprintf(
-			"CREATE TYPE user_practice_status AS ENUM ('%s', '%s', '%s', '%s');",
-			user_practice.Opened,
+			"CREATE TYPE user_practice_status AS ENUM ('%s', '%s', '%s');",
 			user_practice.InProgress,
 			user_practice.Submitted,
 			user_practice.Reviewed,
@@ -105,17 +123,19 @@ func SetupApp() *fiber.App {
 	)
 
 	err = db.AutoMigrate(
+		&endpoint.Endpoint{},
+		&role.Role{},
+		&acl.ACL{},
+		&user.User{},
+		&track.Track{},
+		&user_track.UserTrack{},
 		&content.Content{},
 		&question.Question{},
 		&question_answer_choice.QuestionAnswerChoice{},
-		&role.Role{},
-		&track.Track{},
-		&user.User{},
 		&user_practice.UserPractice{},
 		&user_practice_record.UserPracticeRecord{},
 		&user_project.UserProject{},
 		&user_project_media.UserProjectMedia{},
-		&user_track.UserTrack{},
 	)
 
 	if err != nil {
@@ -124,11 +144,16 @@ func SetupApp() *fiber.App {
 
 	// app.Use(fiberLogger.New())
 
+	middleware := middleware.NewMiddleware(db)
+
 	route := app.Group("/api")
 
 	// Create new repos
 	authRepo := authRepository.NewRepository(db)
 	contentRepo := contentRepository.NewRepository(db)
+	endpointRepo := endpointRepository.NewRepository(db)
+	questionRepo := questionRepository.NewRepository(db)
+	roleRepo := roleRepository.NewRepository(db)
 	trackRepo := trackRepository.NewRepository(db)
 	userMaterialRepo := userMaterialRepository.NewRepository(db)
 	userPracticeRepo := userPracticeRepository.NewRepository(db)
@@ -139,27 +164,36 @@ func SetupApp() *fiber.App {
 
 	// Create new services
 	authSvc := authService.NewService(authRepo, userRepo)
-	contentSvc := contentService.NewService(contentRepo, userPracticeRepo)
+	contentSvc := contentService.NewService(contentRepo, userPracticeRepo, userTrackRepo)
 	practiceSvc := practiceService.NewService(contentRepo)
 	projectSvc := projectService.NewService(contentRepo)
+	questionSvc := questionService.NewService(questionRepo)
 	trackSvc := trackService.NewService(trackRepo)
 	userMaterialSvc := userMaterialService.NewService(userMaterialRepo)
 	userPracticeSvc := userPracticeService.NewService(userPracticeRepo, userPracticeRecordRepo)
-	userProjectSvc := userProjectService.NewService(userProjectRepo)
+	userProjectSvc := userProjectService.NewService(userProjectRepo, contentRepo)
 	userSvc := userService.NewService(userRepo)
 	userTrackSvc := userTrackService.NewService(userTrackRepo)
 
 	// Create new handlers
 	authHandler.NewHandler(route, authSvc)
-	contentHandler.NewHandler(route, contentSvc)
-	practiceHandler.NewHandler(route, practiceSvc)
-	projectHandler.NewHandler(route, projectSvc)
+	contentHandler.NewHandler(route, middleware, contentSvc)
+	practiceHandler.NewHandler(route, middleware, practiceSvc)
+	projectHandler.NewHandler(route, middleware, projectSvc)
+	questionHandler.NewHandler(route, middleware, questionSvc)
 	userMaterialHandler.NewHandler(route, userMaterialSvc)
-	userPracticeHandler.NewHandler(route, userPracticeSvc)
-	userProjectHandler.NewHandler(route, userProjectSvc)
+	userPracticeHandler.NewHandler(route, middleware, userPracticeSvc)
+	userProjectHandler.NewHandler(route, middleware, userProjectSvc)
 	userHandler.NewHandler(route, userSvc)
-	userTrackHandler.NewHandler(route, userTrackSvc)
+	userTrackHandler.NewHandler(route, middleware, userTrackSvc)
 	trackHandler.NewHandler(route, trackSvc)
+
+	seeder.RunSeeders(
+		userRepo,
+		roleRepo,
+		endpointRepo,
+		trackRepo,
+	)
 
 	return app
 }
